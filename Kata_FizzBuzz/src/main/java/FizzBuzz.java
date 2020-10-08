7f3b45012cf3610c3f737ea492198f0ac6e25c9b
Abian Sanchez <AbianSanchezM@gmail.com>
2020-10-08 15:54:07 +0100

    Second test passed

--- Kata_FizzBuzz/src/main/java/FizzBuzz.java
+++ Kata_FizzBuzz/src/main/java/FizzBuzz.java
@@ -5,9 +5,13 @@ public class FizzBuzz {
 
     static String checkFizzBuzz (int value){
         boolean modFive = false;
+        boolean modThree = false;
         if ((value % 5) == 0){ modFive = true;}
+        if ((value % 3) == 0){ modThree = true;}
         if(modFive){
             return "Buzz";
+        } else if (modThree){
+            return "Fizz";
         }
         return ""+value;
     }
