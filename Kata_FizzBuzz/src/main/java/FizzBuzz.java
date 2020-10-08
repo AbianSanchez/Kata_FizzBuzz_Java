4de7a4d167e3b0c82e7d9d63fa1fd38d7f55dab5
Abian Sanchez <AbianSanchezM@gmail.com>
2020-10-08 16:32:43 +0100

    Refactoring and finalized the main execution

--- Kata_FizzBuzz/src/main/java/FizzBuzz.java
+++ Kata_FizzBuzz/src/main/java/FizzBuzz.java
@@ -1,18 +1,19 @@
 public class FizzBuzz {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            System.out.println(checkFizzBuzz(i));
+        }
     }
 
     static String checkFizzBuzz (int value){
-        boolean modFive = false;
-        boolean modThree = false;
-        if ((value % 5) == 0){ modFive = true;}
-        if ((value % 3) == 0){ modThree = true;}
+        boolean modFive = (value % 5) == 0;
+        boolean modThree = (value % 3) == 0;
         if(modFive && modThree){
             return "FizzBuzz";
         }else if(modFive){
             return "Buzz";
-        } else if (modThree){
+        }else if (modThree){
             return "Fizz";
         }
         return ""+value;
